7dc6b393412a32919c53858c9536e3ee27694031
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2024-07-18 15:08:40 +0300

    Add project files.

---
+++ Server_TCP_UDP/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Server_TCP_UDP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
